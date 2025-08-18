078928f978b1f1171b47a4ed2ec9583db524b3bf
linlife <1521052712@qq.com>
2025-08-18 17:06:32 +0800

    fix: add ci_no_aws provider option

--- providers.tf
+++ providers.tf
@@ -1,5 +1,17 @@
-variable "region" { type = string }
+variable "region" {
+  type = string
+}
+
+variable "ci_no_aws" {
+  type    = bool
+  default = false
+}
 
 provider "aws" {
-  region = var.region
+  region                      = var.region
+  skip_credentials_validation = var.ci_no_aws
+  skip_requesting_account_id  = var.ci_no_aws
+  skip_metadata_api_check     = var.ci_no_aws
+  skip_region_validation      = var.ci_no_aws
+
 }
